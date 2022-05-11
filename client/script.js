4a17dcf94ac9b5020b5cdfc158c5af71f27608bd
William <williamsessions23@gmail.com>
2022-05-11 14:43:24 +0100

    made a lot of things work, finished design and added functionality for both apis

--- client/script.js
+++ client/script.js
@@ -1,53 +1,98 @@
 let google = document.querySelector(".search-btn");
 let lucky = document.querySelector(".lucky-btn");
-
+let logo = document.querySelector(".logo-search");
 let input = document.querySelector("input");
 
 let googleSwitch = document.querySelector("#switch");
 
-let accessKey = "b66dea11ca6dce22d64c88699e872510";
+let searchResults = document.querySelector(".search-results");
 
-google.addEventListener("click", () => {
-  googleSearch();
-  apiSearch();
+let googleContainer = document.querySelector(".container");
+
+let magnifySearch = document.querySelectorAll(".magnify");
+
+logo.addEventListener("click", (e) => {
+  googleContainer.classList.remove("none");
+  searchResults.classList.add("none");
 });
+
+magnifySearch.forEach((magnify) => {
+  magnify.addEventListener("click", searchHelper);
+});
+
+google.addEventListener("click", searchHelper);
 document.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    googleSearch();
-    apiSearch();
+    searchHelper();
   }
 });
+
+lucky.addEventListener("click", feelingLucky);
+
+function searchHelper() {
+  googleContainer.classList.add("none");
+  searchResults.classList.remove("none");
+  removeResults();
+  googleSearch();
+  apiSearch();
+}
 
 function googleSearch() {
   if (googleSwitch.checked) {
     let query = input.value;
-
-    console.log(query);
+    let accessKey = "b66dea11ca6dce22d64c88699e872510";
 
     let url =
-    "http://api.serpstack.com/search?access_key=" +
-    accessKey +
-    "&query=" +
-    query;
+      "http://api.serpstack.com/search?access_key=" +
+      accessKey +
+      "&query=" +
+      query;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        data.organic_results.forEach((element) => {
+          searchResults.insertAdjacentHTML(
+            "beforeend",
+            `<div class="result">
+        <div class="url">${element.url}</div>
+        <div>
+          <a class="link" href="${element.url}">${element.title}</a>
+        </div>
+        <div class="snippet">
+          ${element.snippet}
+        </div>
+      </div>`
+          );
+        });
 
-      data.organic_results.forEach((element) => {
-        console.log(element.title);
-        console.log(element.snippet);
-        console.log(element.url);
+        data.organic_results;
       });
-
-      data.organic_results;
-    }); 
-  }
+  } else return;
 }
 
-
-lucky.addEventListener("click", feelingLucky);
+function apiSearch() {
+  if (!googleSwitch.checked) {
+    fetch("http://localhost:3000/search/")
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach((element) => {
+          searchResults.insertAdjacentHTML(
+            "beforeend",
+            `<div class="result">
+          <div class="url">${element.url}</div>
+          <div>
+            <a class="link" href="${element.url}">${element.title}</a>
+          </div>
+          <div class="snippet">
+            ${element.snippet}
+          </div>
+        </div>`
+          );
+        });
+      });
+  } else return;
+}
 
 function feelingLucky() {
   fetch("http://localhost:3000/search/random")
@@ -55,24 +100,12 @@ function feelingLucky() {
     .then((data) => {
       window.location.replace(data);
     });
-
-function apiSearch() {
-    if (!googleSwitch.checked) {
-
-    fetch('http://localhost:3000/search/')
-    .then((response) => response.json())
-    .then((data) => {
-      
-
-        data.forEach((element) => {
-            console.log(element.title);
-            console.log(element.snippet);
-            console.log(element.url);
-        });
-
-    
-    })
-
 }
 
+function removeResults() {
+  const results = document.querySelectorAll(".result");
+
+  results.forEach((result) => {
+    result.remove();
+  });
 }
