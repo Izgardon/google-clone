45aac5b8f1658397f5d097f12ddf524bf4880294
William <williamsessions23@gmail.com>
2022-05-11 11:36:42 +0100

    google page done and linked to google api

--- client/script.js
+++ client/script.js
@@ -1,24 +1,49 @@
+let google = document.querySelector(".search-btn");
+let lucky = document.querySelector(".lucky-btn");
+
+let input = document.querySelector("input");
+
+let googleSwitch = document.querySelector("#switch");
 
 let accessKey = "b66dea11ca6dce22d64c88699e872510";
-let query = 'Mcdonalds'; //defined from searchbar 
 
-let url = 'http://api.serpstack.com/search?access_key='+accessKey+"&query="+query
+google.addEventListener("click", () => {
+  googleSearch();
+  apiSearch();
+});
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    googleSearch();
+    apiSearch();
+  }
+});
 
-fetch(url)
-.then(response =>response.json())
-.then(data =>{    
-    console.log(data);
+function googleSearch() {
+  if (googleSwitch.checked) {
+    let query = input.value;
 
-    data.organic_results.forEach(element => {
+    console.log(query);
+
+    /* let url =
+    "http://api.serpstack.com/search?access_key=" +
+    accessKey +
+    "&query=" +
+    query;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      data.organic_results.forEach((element) => {
         console.log(element.title);
         console.log(element.snippet);
         console.log(element.url);
-        
-    });
+      });
 
-    data.organic_results
-})
+      data.organic_results;
+    }); */
+  }
+}
 
-let google = document.querySelector();
-let lucky = document.querySelector;
-
+function apiSearch() {}
